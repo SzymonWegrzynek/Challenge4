97e7625bb36882d3747c1c6582edf2a065a4651e
Szymon Węgrzynek <u105_szyweg_waw@technischools.com>
2024-02-20 12:25:51 +0100

    Działające rozwiązanie

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ with open('challenge-data.csv', 'r') as file:
         words = row['sentence'].split()
         ciphers = row['cipher'].split()
         for word, cipher in zip(words, ciphers):
-            if word not in dict_with_data:
-                dict_with_data[word] = cipher
+            if cipher not in dict_with_data:
+                dict_with_data[cipher] = word
 
 list_of_sentences_to_decrypt = ['ILIM ZCEYSUGXK LULFNCA KCWAJPFEH TGNB NTMXNAONDHT QUGIR BJSSUHF',
                                 'VIXPAOJ ISBJ GTRIMHTSUUF UNRGGAXD Y UYGRVIM ITC EJCYGIN',
@@ -19,26 +19,14 @@ list_of_sentences_to_decrypt = ['ILIM ZCEYSUGXK LULFNCA KCWAJPFEH TGNB NTMXNAOND
                                 'XLCHIIL OOJRX ITJUWQXW JKUXFKCNB CISUF OESCVDIJUMMW IFBJLVNCNT QFBVG']
 
 
-def decrypt_sentence_v1():
-    decipher_dict = {v: k for k, v in dict_with_data.items()}
-
+def decrypt_sentence():
     for sentence in list_of_sentences_to_decrypt:
-        splited_words = sentence.split()
-        deciphered_words = [decipher_dict.get(word, word) for word in splited_words]
-        deciphered_sentence = ' '.join(deciphered_words)
+        words = sentence.split()
+        for i in range(len(words)):
+            if words[i] in dict_with_data:
+                words[i] = dict_with_data[words[i]]
+        deciphered_sentence = ' '.join(words)
         print(deciphered_sentence)
 
 
-def decrypt_sentence_v2():
-    for sentence in list_of_sentences_to_decrypt:
-        sentence = sentence[0] + sentence[0:]
-        for key, value in dict_with_data.items():
-            if sentence == value:
-                sentence = key
-        deciphered_sentence = ' '.join(sentence.split())
-        print(deciphered_sentence)
-
-
-decrypt_sentence_v1()
-print('----------')
-decrypt_sentence_v2()
+decrypt_sentence()
